b6a383a41706a87e9908a3693d4466bfa400fb41
Emmanuel Sciara <emmanuel.sciara@gmail.com>
2014-04-04 01:08:45 +0200

    recipe written and all tests passed

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,13 @@
 # 
 # All rights reserved - Do Not Redistribute
 #
+
+package 'lighttpd'
+
+service 'lighttpd' do
+  action [:enable, :start]
+end
+
+cookbook_file '/var/www/index.html' do
+  source 'wonderstuff.html'
+end
