fea982d164c529f603b79cc099d858b5a4bf2bad
Emmanuel Sciara <emmanuel.sciara@gmail.com>
2014-04-03 23:39:09 +0200

    now with integration tests with serverspec

---
+++ test/integration/default/serverspec/localhost/readable_services_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe 'Wonderstuff Design' do
+
+  it 'should install the lighttpd package' do
+    expect(package 'lighttpd').to be_installed
+  end
+
+  it 'should enable and start the lighttpd service' do
+    expect(service 'lighttpd').to be_enabled
+    expect(service 'lighttpd').to be_running
+  end
+
+  it 'should render the Wonderstuff Design web page' do
+    expect(file('/var/www/index.html')).to be_file
+    expect(file('/var/www/index.html')).to contain 'Wonderstuff Design is a boutique graphics design agency.'
+  end
+
+end
