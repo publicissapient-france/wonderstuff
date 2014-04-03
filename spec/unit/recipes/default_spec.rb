02138da3f599f3413895e024c5b46b9872592c4e
Emmanuel Sciara <emmanuel.sciara@gmail.com>
2014-04-04 00:18:01 +0200

    added the unit tests... which fail at the moment :)

---
+++ spec/unit/recipes/default_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe "wonderstuff::default" do
+  let(:chef_run) do
+    runner = ChefSpec::ChefRunner.new(
+      log_level: :error,
+      cookbook_path: COOKBOOK_PATH,
+    )
+    Chef::Config.force_logger true
+    runner.converge('recipe[wonderstuff::default]')
+  end
+
+  it "installs the lighttpd package" do
+    expect(chef_run).to install_package 'lighttpd'
+  end
+
+  it "creates a webpage to be served" do
+    expect(chef_run).to create_file_with_content '/var/www/index.html', 'Wonderstuff Design is a boutique graphics design agency.'
+  end
+
+  it "starts the lighttpd service" do
+    expect(chef_run).to start_service 'lighttpd'
+  end
+
+  it "enables the lighttpd service" do
+    expect(chef_run).to set_service_to_start_on_boot 'lighttpd'
+  end
+end
